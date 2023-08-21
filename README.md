2f26863594ee2a886c48dcba6b158d9cf9159186
ashish-greycube <mr.ashish.shah@gmail.com>
2023-08-21 14:53:37 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Soknana Maintenance
+
+Customization for Soknana
+
+#### License
+
+MIT
---
+++ soknana_maintenance/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Soknana Maintenance",
+			"type": "module",
+			"label": _("Soknana Maintenance")
+		}
+	]
