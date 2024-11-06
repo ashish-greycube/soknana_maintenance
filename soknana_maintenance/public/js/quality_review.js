f22bb84436d9dc30230a14d3a9bc7a827ca2b866
ashish-greycube <mr.ashish.shah@gmail.com>
2024-11-06 19:25:21 +0530

    fix: make attachment readonly for other than quality manager

--- soknana_maintenance/public/js/quality_review.js
+++ soknana_maintenance/public/js/quality_review.js
@@ -1,5 +1,18 @@
 frappe.ui.form.on('Quality Review', {
 	refresh: function(frm) {
         frm.meta.make_attachments_public=1
+        let supervisor_review_attachment_read_only=1
+        
+        if (frappe.user.has_role("Quality Manager")) {
+            supervisor_review_attachment_read_only=0
+        }        
+        // if (frappe.user.has_role("System Manager")) {
+        //     supervisor_review_attachment_read_only=0
+        // }
+        for (let index = 0; index < frm.doc.reviews.length; index++) {
+            const element = frm.doc.reviews[index];
+            frm.set_df_property('reviews', 'read_only', supervisor_review_attachment_read_only, frm.docname, 'custom_supervisor_review_attachment', element.name)
+            
+        }        
     }
 })
